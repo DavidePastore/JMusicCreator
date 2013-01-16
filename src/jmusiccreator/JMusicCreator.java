3e519e740e78ad786c3e4c7d1f0502ee25e4431e
DavidePastore <pasdavide@gmail.com>
2013-01-16 19:40:35 +0100

    Transform the project into a java project

---
+++ src/jmusiccreator/JMusicCreator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jmusiccreator;
+
+/**
+ * @author <a reef="https://github.com/DavidePastore">DavidePastore</a>
+ * The application starts here.
+ */
+public class JMusicCreator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
